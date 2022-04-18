eefe145ebe57984372922865b1bdf78691016e21
lazarobeas <39280428+lazarobeas@users.noreply.github.com>
2022-04-18 11:15:55 -0500

    Fixed Version Hangman

--- hangman/main.py
+++ hangman/main.py
@@ -40,27 +40,27 @@ while num_of_incorrect_guesses < 5 and False in answer_guesses:
 
   print()
 
-letter = input("Enter a letter: ")
+  letter = input("Enter a letter: ")
 
-letter = letter.upper()
+  letter = letter.upper()
       
-if letter not in guessed_letters and len(letter) == 1 and re.search("^[A-Z]$", letter):
-  guessed_letters_insert_index = 0
+  if letter not in guessed_letters and len(letter) == 1 and re.search("^[A-Z]$", letter):
+    guessed_letters_insert_index = 0
 
-  for current_guessed_letter in guessed_letters:
-    if letter < current_guessed_letter:
-      break
+    for current_guessed_letter in guessed_letters:
+      if letter < current_guessed_letter:
+        break
 
-    guessed_letters_insert_index += 1
+      guessed_letters_insert_index += 1
 
     guessed_letters.insert(guessed_letters_insert_index, letter)
-  if letter in answer_characters:
+    if letter in answer_characters:
     #Letter is in the puzzle
-    for current_answer_index in range(len(answer_characters)):
-      if letter == answer_characters[current_answer_index]:
-        answer_guesses[current_answer_index ] = True
-      else:
-        num_of_incorrect_guesses += 1
+      for current_answer_index in range(len(answer_characters)):
+        if letter == answer_characters[current_answer_index]:
+          answer_guesses[current_answer_index ] = True
+    else:
+      num_of_incorrect_guesses += 1
 
 # Game is over. Tells whether students won or not.
 if num_of_incorrect_guesses < 5:
